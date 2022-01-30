a6cd1fefe7d0128b784aa679c9b003dbaef4b08c
Simeon Akanbi <simeonakanbi99@gmail.com>
2022-01-30 13:34:38 +0100

    Made the User section a react application

--- User/Src/index.js
+++ User/Src/index.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+const Index = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    );
+};
+
+render(<Index />, document.getElementById('js_base'));
